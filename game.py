4c139e42b4f4ac047376f061c8db5b127cbbe2a8
zonel <bartlomiej.bobowski@microsoft.wsei.edu.pl>
2023-12-12 12:43:38 +0100

    [Refactor] used design patterns for main logic of the app

--- game.py
+++ game.py
@@ -38,138 +38,134 @@ import pygame
 import numpy as np
 import pickle
 
-# Initialize Pygame
-pygame.init()
+class Button:
+    def __init__(self, x, y, width, height, text, action):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.text = text
+        self.action = action
 
-# Screen dimensions
-width, height = 800, 600
-screen = pygame.display.set_mode((width, height))
+    def draw(self, screen):
+        pygame.draw.rect(screen, (0, 255, 0), self.rect)
+        font = pygame.font.Font(None, 36)
+        text = font.render(self.text, True, (0, 0, 0))
+        text_rect = text.get_rect(center=self.rect.center)
+        screen.blit(text, text_rect)
 
-# Grid dimensions
-n_cells_x, n_cells_y = 40, 30
-cell_width = width // n_cells_x
-cell_height = height // n_cells_y
+class InfoButton:
+    def __init__(self, x, y, width, height, text):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.text = text
 
-# Game state
-game_state = np.random.choice([0, 1], size=(n_cells_x, n_cells_y), p=[0.8, 0.2])
+    def draw(self, screen):
+        pygame.draw.rect(screen, (0, 102, 255), self.rect)
+        font = pygame.font.Font(None, 24)
+        text = font.render(self.text, True, (255, 255, 255))
+        text_rect = text.get_rect(center=self.rect.center)
+        screen.blit(text, text_rect)
 
-# Colors
-white = (255, 255, 255)
-black = (0, 0, 0)
-gray = (128, 128, 128)
-green = (0, 255, 0)
-blue = (0,102,255)
+class GameState:
+    def __init__(self, n_cells_x, n_cells_y):
+        self.n_cells_x = n_cells_x
+        self.n_cells_y = n_cells_y
+        self.game_state = np.random.choice([0, 1], size=(n_cells_x, n_cells_y), p=[0.8, 0.2])
 
-# Button dimensions
-button_width, button_height = 200, 50
-button_x, button_y = (width - button_width) // 2, height - button_height - 10
+    def next_generation(self):
+        new_state = np.copy(self.game_state)
+        for y in range(self.n_cells_y):
+            for x in range(self.n_cells_x):
+                n_neighbors = np.sum(self.game_state[x-1:x+2, y-1:y+2]) - self.game_state[x, y]
+                if self.game_state[x, y] == 1 and (n_neighbors < 2 or n_neighbors > 3):
+                    new_state[x, y] = 0
+                elif self.game_state[x, y] == 0 and n_neighbors == 3:
+                    new_state[x, y] = 1
+        self.game_state = new_state
 
-def draw_button():
-    pygame.draw.rect(screen, green, (button_x, button_y, button_width, button_height))
-    font = pygame.font.Font(None, 36)
-    text = font.render("Start/Stop", True, black)
-    text_rect = text.get_rect(center=(button_x + button_width // 2, button_y + button_height // 2))
-    screen.blit(text, text_rect)
+class Grid:
+    def __init__(self, width, height, n_cells_x, n_cells_y):
+        self.width = width
+        self.height = height
+        self.cell_width = width // n_cells_x
+        self.cell_height = height // n_cells_y
 
-def draw_info_text():
-    font = pygame.font.Font(None, 24)
-    text = font.render("CTRL+S to Save | CTRL+L to Load game", True, white)
-    text_rect = text.get_rect(center=(width // 2, height - button_height - 30))
-    
-    # Create a rectangle behind the text
-    rect_width = text_rect.width + 20
-    rect_height = text_rect.height + 15
-    rect_x = (width - rect_width) // 2
-    rect_y = height - button_height - 40
-    
-    pygame.draw.rect(screen, blue, (rect_x, rect_y, rect_width, rect_height))
-    screen.blit(text, text_rect)
+    def draw(self, screen):
+        for y in range(0, self.height, self.cell_height):
+            for x in range(0, self.width, self.cell_width):
+                cell = pygame.Rect(x, y, self.cell_width, self.cell_height)
+                pygame.draw.rect(screen, (128, 128, 128), cell, 1)
 
-def draw_grid():
-    for y in range(0, height, cell_height):
-        for x in range(0, width, cell_width):
-            cell = pygame.Rect(x, y, cell_width, cell_height)
-            pygame.draw.rect(screen, gray, cell, 1)
+class Game:
+    def __init__(self, width, height, n_cells_x, n_cells_y):
+        pygame.init()
+        self.screen = pygame.display.set_mode((width, height))
+        self.grid = Grid(width, height, n_cells_x, n_cells_y)
+        self.game_state = GameState(n_cells_x, n_cells_y)
+        self.button = Button((width - 200) // 2, height - 60, 485, 50, "Start/Stop", self.toggle_pause)
+        self.info_button = InfoButton(10, height - 60, 400, 50, "CTRL+S to Save | CTRL+L to Load game")
+        self.paused = False
+        self.tick_interval = 0.1
+        self.last_tick = pygame.time.get_ticks()
 
-def next_generation():
-    global game_state
-    new_state = np.copy(game_state)
+    def run(self):
+        running = True
+        while running:
+            current_time = pygame.time.get_ticks()
+            delta_time = (current_time - self.last_tick) / 1000.0
 
-    for y in range(n_cells_y):
-        for x in range(n_cells_x):
-            n_neighbors = game_state[(x - 1) % n_cells_x, (y - 1) % n_cells_y] + \
-                          game_state[(x)     % n_cells_x, (y - 1) % n_cells_y] + \
-                          game_state[(x + 1) % n_cells_x, (y - 1) % n_cells_y] + \
-                          game_state[(x - 1) % n_cells_x, (y)     % n_cells_y] + \
-                          game_state[(x + 1) % n_cells_x, (y)     % n_cells_y] + \
-                          game_state[(x - 1) % n_cells_x, (y + 1) % n_cells_y] + \
-                          game_state[(x)     % n_cells_x, (y + 1) % n_cells_y] + \
-                          game_state[(x + 1) % n_cells_x, (y + 1) % n_cells_y]
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    self.handle_click(event.pos)
+                elif event.type == pygame.KEYDOWN:
+                    self.handle_key(event.key)
 
-            if game_state[x, y] == 1 and (n_neighbors < 2 or n_neighbors > 3):
-                new_state[x, y] = 0
-            elif game_state[x, y] == 0 and n_neighbors == 3:
-                new_state[x, y] = 1
+            if not self.paused and delta_time >= self.tick_interval:
+                self.last_tick = current_time
+                self.game_state.next_generation()
 
-    game_state = new_state
+            self.screen.fill((255, 255, 255))
+            self.grid.draw(self.screen)
+            self.draw_cells()
+            self.button.draw(self.screen)
+            self.info_button.draw(self.screen)
+            pygame.display.flip()
 
-def draw_cells():
-    for y in range(n_cells_y):
-        for x in range(n_cells_x):
-            cell = pygame.Rect(x * cell_width, y * cell_height, cell_width, cell_height)
-            if game_state[x, y] == 1:
-                pygame.draw.rect(screen, black, cell)
+        pygame.quit()
 
-running = True
-paused = False
-tick_interval = 0.5  # Time interval between ticks in seconds
-last_tick = pygame.time.get_ticks()  # Time tracking for simulation
+    def handle_click(self, pos):
+        if self.button.rect.collidepoint(pos):
+            self.button.action()
 
-def save_game_state(filename):
-    with open(filename, 'wb') as file:
-        pickle.dump(game_state, file)
+        else:
+            x, y = pos[0] // self.grid.cell_width, pos[1] // self.grid.cell_height
+            self.game_state.game_state[x, y] = not self.game_state.game_state[x, y]
 
-def load_game_state(filename):
-    global game_state
-    with open(filename, 'rb') as file:
-        game_state = pickle.load(file)
+    def handle_key(self, key):
+        if key == pygame.K_s and pygame.key.get_mods() & pygame.KMOD_CTRL:
+            self.save_game_state("saved_game_state.pickle")
+        elif key == pygame.K_l and pygame.key.get_mods() & pygame.KMOD_CTRL:
+            self.load_game_state("saved_game_state.pickle")
 
-def toggle_pause():
-    global paused
-    paused = not paused
+    def draw_cells(self):
+        for y in range(self.game_state.n_cells_y):
+            for x in range(self.game_state.n_cells_x):
+                cell = pygame.Rect(x * self.grid.cell_width, y * self.grid.cell_height,
+                                   self.grid.cell_width, self.grid.cell_height)
+                if self.game_state.game_state[x, y] == 1:
+                    pygame.draw.rect(self.screen, (0, 0, 0), cell)
 
-running = True
-while running:
-    current_time = pygame.time.get_ticks()
-    # Calculate time passed since the last tick
-    delta_time = (current_time - last_tick) / 1000.0  # Convert milliseconds to seconds
+    def toggle_pause(self):
+        self.paused = not self.paused
 
-    if not paused and delta_time >= tick_interval:
-        last_tick = current_time
-        next_generation()
+    def save_game_state(self, filename):
+        with open(filename, 'wb') as file:
+            pickle.dump(self.game_state.game_state, file)
 
-    screen.fill(white)
-    draw_grid()
-    draw_cells()
-    draw_button()
-    draw_info_text()
-    pygame.display.flip()
+    def load_game_state(self, filename):
+        with open(filename, 'rb') as file:
+            self.game_state.game_state = pickle.load(file)
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if button_x <= event.pos[0] <= button_x + button_width and button_y <= event.pos[1] <= button_y + button_height:
-                toggle_pause()
-            else:
-                x, y = event.pos[0] // cell_width, event.pos[1] // cell_height
-                game_state[x, y] = not game_state[x, y]
+game = Game(800, 600, 40, 30)
+game.run()
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_s and pygame.key.get_mods() & pygame.KMOD_CTRL:
-                save_game_state("saved_game_state.pickle")
-            elif event.key == pygame.K_l and pygame.key.get_mods() & pygame.KMOD_CTRL:
-                load_game_state("saved_game_state.pickle")
-
-pygame.quit()
 
